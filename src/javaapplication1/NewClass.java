997e98b2cb64de3d2e6e5dfc6d7646b0e8d7e277
Chang Chen Ong <twt6m@hotmail.com>
2019-04-04 10:21:33 +0100

    First push

---
+++ src/javaapplication1/NewClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author User
+ */
+public class NewClass {
+    public void hello()
+    {
+        System.out.println("I am new!");
+    }
+    public void addition (int a, int b)
+    {
+        int result = a + b;
+        System.out.println(result);
+    }
+}
